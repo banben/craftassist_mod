908fdc16d726131ca3caa71391d532b27b1bda0b
vvv <vvv@ss.com>
2020-08-07 01:02:19 +0800

    test

--- cuberite_plugins/given_spawn_position/given_spawn_position.lua
+++ cuberite_plugins/given_spawn_position/given_spawn_position.lua
@@ -23,7 +23,7 @@ function OnPlayerSpawned(player)
     local Roll = 0
     local Pitch = 29.399936676025
     local Yaw = -89.398803710938
-    player:Freeze(Vector3d(x, y, z))
+    -- player:Freeze(Vector3d(x, y, z))
     player:SetRoll(Roll)
     player:SetPitch(Pitch)
     player:SetYaw(Yaw)
