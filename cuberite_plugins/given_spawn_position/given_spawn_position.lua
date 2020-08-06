f4213e59f7105a0745600cc8ed5aa4cf843f3784
vvv <vvv@ss.com>
2020-08-06 16:05:09 +0800

    test

---
+++ cuberite_plugins/given_spawn_position/given_spawn_position.lua
@@ -0,0 +1,23 @@
+-- Copyright (c) Facebook, Inc. and its affiliates.
+
+
+PLUGIN = nil
+
+function Initialize(Plugin)
+    Plugin:SetName("GivenStartPosition")
+    Plugin:SetVersion(1)
+
+    PLUGIN = Plugin
+    cPluginManager.AddHook(cPluginManager.HOOK_PLAYER_SPAWNED, OnPlayerSpawned)
+
+    LOG("Loaded plugin: GivenStartPosition")
+    math.randomseed(0)
+    return true
+end
+
+function OnPlayerSpawned(player)
+    x = 0.50
+    y = 63.00
+    z = 0.50
+    player:TeleportToCoords(x, y, z)
+end
